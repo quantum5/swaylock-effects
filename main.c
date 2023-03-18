df957f1a9f6bb6176da73797ea20a788d7c0bc8e
lx200916 <mjt197305@gmail.com>
2023-03-18 21:19:00 +0800

    Fix red screen even through Swaylock exit normally
    Seems like if Swaylock exits before Wayland handles
    `ext_session_lock_v1_unlock_and_destroy` and free the state then Wayland
    may treat this as a crash then cause Red Screen.

--- main.c
+++ main.c
@@ -1980,6 +1980,7 @@ int main(int argc, char **argv) {
 	}
 	if (state.ext_session_lock_v1) {
 		ext_session_lock_v1_unlock_and_destroy(state.ext_session_lock_v1);
+		wl_display_roundtrip(state.display);
 		wl_display_flush(state.display);
 	}
 
